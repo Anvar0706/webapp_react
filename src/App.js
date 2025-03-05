b5f59afbefd83853659fa8190e0ec7ec588e9411
Anvar0706 <97040968+Anvar0706@users.noreply.github.com>
2025-03-05 08:06:51 +0500

    Fayllar yangilandi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { useEffect } from "react";
 
+const tg = window.Telegram.WebApp
+
 function App() {
   useEffect(() => {
     tg.ready();
   }, [])
-
   return (
     <div className="App">
       <h1>App</h1>
