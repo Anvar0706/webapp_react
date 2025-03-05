6f2563152b0971f1a43d172760996f84d864b38e
Anvar0706 <97040968+Anvar0706@users.noreply.github.com>
2025-03-05 08:03:00 +0500

    Fayllar yangilandi

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
 import { useEffect } from "react";
 
-const tg = window.Telegram.WebApp
-
 function App() {
   useEffect(() => {
     tg.ready();
   }, [])
-  const onClose = () => {
-  tg.close();
-  }
 
   return (
     <div className="App">
-      <button onClick ={onClose}>Exit</button>
       <h1>App</h1>
     </div>
   );
